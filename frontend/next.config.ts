982d5ff7133c74fc03e9950156f48cce66bdd9ba
mikepn02 <nzaberamikepeter@gmail.com>
2025-11-02 10:00:00 +0200

    (feat): updated dockerfile

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['*'],
+    },
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_LOCAL_STORAGE_KEY: process.env.NEXT_PUBLIC_LOCAL_STORAGE_KEY,
+    NEXT_PUBLIC_SESSION_KEY_KEY: process.env.NEXT_PUBLIC_SESSION_KEY_KEY
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
